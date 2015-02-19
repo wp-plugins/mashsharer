36f1ccf353ea190c4aa2bfef4e77622f1311394a
ReneHermi <ReneHermi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-19 15:59:39 +0000

    update admin settings
    git-svn-id: https://plugins.svn.wordpress.org/mashsharer/tags/2.2.6@1094448 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin/settings/register-settings.php
+++ includes/admin/settings/register-settings.php
@@ -249,7 +249,7 @@ function mashsb_get_registered_settings() {
                             'mashsharer_position' => array(
 					'id' => 'mashsharer_position',
 					'name' => __( 'Position', 'mashsb' ),
-					'desc' => __( 'Choose where you would like the social icons to appear, before or after the main content. If set to Manual, you can use this code to place your Social links anywhere you like in your templates files: <strong>&lt;?php do_action("mashshare"); ?&gt;</strong> or use the shortcode: [mashshare] in your posts. Optional: <strong>[mashshare shares="false"]</strong> if you like to disable the share number.', 'mashsb' ),
+					'desc' => __( 'Choose where you would like the social icons to appear, before or after the main content. If set to Manual, you can use this code to place your Social links anywhere you like in your templates files: <strong>&lt;?php do_shortcode("[mashshare]"); ?&gt;</strong> or use the shortcode: [mashshare] in your posts. Optional: <strong>[mashshare shares="false"]</strong> if you like to disable the share number. See all <a href="https://www.mashshare.net/faq/#Is_there_a_shortcode_for_pages_and_posts" target="_blank">possible shortcodes</a> here.', 'mashsb' ),
 					'type' => 'select',
                                         'options' => array(
 						'before' => __( 'Top', 'mashsb' ),
